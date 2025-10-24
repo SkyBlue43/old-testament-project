b694169e7ecbb2cabc20e347ad1687fabb754754
Skyler <SkyBlue43@users.noreply.github.com>
2025-10-24 10:57:05 -0600

    Got the tailwind to work

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 import "./App.css";
 
 function App() {
-  return <h1 className="text-3xl font-bold text-blue-500">Hello Tailwind!</h1>;
+  return (
+    <div className="flex w-screen h-screen overflow-hidden">
+      <div className="w-1/5 bg-red-700">left</div>
+      <div className="flex-1">middle</div>
+      <div className="w-1/5 bg-red-700">right</div>
+    </div>
+  );
 }
 
 export default App;
