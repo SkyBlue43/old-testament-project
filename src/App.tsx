7f0f9083d9d78799dc5ba37d106a403eadf92cd5
Skyler <SkyBlue43@users.noreply.github.com>
2025-10-25 17:29:55 -0600

    Main layout in place

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,18 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="flex w-screen h-screen overflow-hidden">
-      <div className="w-1/5 bg-red-700">left</div>
-      <div className="flex-1">middle</div>
-      <div className="w-1/5 bg-red-700">right</div>
+    <div className="flex w-screen h-screen overflow-hidden bg-gray-200 text-black">
+      <div className="flex flex-col w-1/5">
+        <div className="h-1/5 bg-red-400 rounded-lg shadow-lg transition-transform duration-200 ease-in-out hover:scale-105 hover:shadow-2xl"></div>
+        <div className="h-4/5 bg-red-300 rounded-lg shadow-lg transition-transform duration-200 ease-in-out hover:scale-105 hover:shadow-2xl"></div>
+      </div>
+      <div className="flex-1 justify-center text-center m-8 text-4xl">
+        Welcome to the Old Testament Project!
+      </div>
+      <div className="w-1/5">
+        <div className="h-1/5 bg-red-400 rounded-lg shadow-lg transition-transform duration-200 ease-in-out hover:scale-105 hover:shadow-2xl"></div>
+        <div className="h-4/5 bg-red-300 rounded-lg shadow-lg transition-transform duration-200 ease-in-out hover:scale-105 hover:shadow-2xl"></div>
+      </div>
     </div>
   );
 }
